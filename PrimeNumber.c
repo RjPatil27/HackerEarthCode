dae605e6d65bf0cd794e0415fe1f122bb3100c60
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2018-02-18 10:31:55 +0000

    Create PrimeNumber.c

---
+++ PrimeNumber.c
@@ -0,0 +1,49 @@
+/*
+You are given an integer N. You need to print the series of all prime numbers till N.
+
+Input Format
+
+The first and only line of the input contains a single integer N denoting the number till where you need to find the series of prime number.
+
+Output Format
+
+Print the desired output in single line separated by spaces.
+
+Constraints
+
+1<=N<=1000
+
+INPUT:
+9
+
+OUTPUT:
+2 3 5 7
+
+*/
+
+//CODE in C.
+
+ #include<stdio.h>
+
+int main(){
+
+    int num,i,count,N;
+    scanf("%d",&N);
+
+    for(num = 1;num<=N;num++){
+
+         count = 0;
+
+         for(i=2;i<=num/2;i++){
+             if(num%i==0){
+                 count++;
+                 break;
+             }
+        }
+        
+         if(count==0 && num!= 1)
+             printf("%d ",num);
+    }
+  
+   return 0;
+}
