7c5e51b6be2b28f3a5fdc68afaec22bb7e29f38e
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2018-02-19 17:41:54 +0000

    Create ASCIIvalue.c

---
+++ ASCIIvalue.c
@@ -0,0 +1,21 @@
+/*
+Given a character C
+print the ASCII value of that character.
+
+Input:
+First and only line in input contains a character C
+
+Output:
+Print the ASCII value of the character C
+*/
+
+//Code written in C.
+
+#include<stdio.h>
+int main()
+{
+  char ch; 
+  scanf("%c",&ch);
+  printf("%d",ch); 
+  return 0;
+}
