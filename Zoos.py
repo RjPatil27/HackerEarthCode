b3b9030c462f58adcf8cacfdf3d857dec9f7243d
Rajat Patil <patilrajat277@gmail.com>
2021-08-11 12:33:42 -0500

    Create Zoos.py

---
+++ Zoos.py
@@ -0,0 +1,22 @@
+'''
+Problem
+You are required to enter a word that consists of  and  that denote the number of Zs and Os respectively. 
+The input word is considered similar to word zoo if,
+2 * x = y
+
+Determine if the entered word is similar to word zoo.
+
+For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.
+
+Input format:
+First line: A word that starts with several Zs and continues by several Os.
+Note: The maximum length of this word must be 20.
+
+Eg,
+Input - zzzoooooo
+Output - Yes
+'''
+
+
+
+
